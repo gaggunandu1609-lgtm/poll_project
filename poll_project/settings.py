92d293224f1273bb4d7dcbddb8dfa48c9c8cc7db
gaggu <gaggunandu1609@gmail.com>
2026-02-26 20:37:47 +0530

    production settings

--- poll_project/settings.py
+++ poll_project/settings.py
@@ -6,11 +6,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY
-SECRET_KEY = 'django-insecure-change-this-key'
-
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
+SECRET_KEY = os.environ.get('SECRET_KEY')
+DEBUG = os.environ.get('DEBUG') == 'True'
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
 
 
 # Applications
