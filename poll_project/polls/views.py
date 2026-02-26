83abc972ea1ce84497474421fcd501df3c4b08f5
gaggu <gaggunandu1609@gmail.com>
2026-02-26 19:57:31 +0530

    md

---
+++ poll_project/polls/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Question, Choice
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+
+# INDEX PAGE
+def index(request):
+    latest_question_list = Question.objects.all()
+
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return render(request, 'polls/index.html', context)
+
+
+# DETAIL PAGE
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+# RESULT PAGE
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+
+# VOTE FUNCTION
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+
+    try:
+        selected_choice = question.choice_set.get(
+            pk=request.POST['choice']
+        )
+    except:
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice."
+        })
+
+    selected_choice.votes += 1
+    selected_choice.save()
+
+    return HttpResponseRedirect(
+        reverse('polls:results', args=(question.id,))
+    )
